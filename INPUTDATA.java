acc41ef65dd15cbf7628473373cb321995572c18
Minahilamjadd <minahilamjad210@gmail.com>
2025-02-13 16:53:21 +0500

    Add files via upload

---
+++ INPUTDATA.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class INPUTDATA{
+         public static void main(String args[]){
+                      Scanner sc = new Scanner(System.in);
+
+                      int i=0;
+                      
+                      System.out.println("enter integer : ");
+                      i = sc.nextInt();
+                      System.out.printf("Value entered : %d\n",i);
+                      
+                      //sc.nextByte();
+                      //sc.nextShort();
+                      //not applicable to character 
+                      
+                      //sc.next(); //for single word
+                      
+                   
+}
+}
---
+++ PrimitiveTypes.java
@@ -0,0 +1,34 @@
+public class PrimitiveTypes{
+             public static void main(String args[]){
+                         
+                         byte b=100;
+                         short s=5;
+                         int i=10;
+                         long l=300;
+
+                         float f=10.6f;
+                         double d=400.0;
+                         
+                         char ch='c';
+                         boolean bool = true; // false
+                         System.out.printf("%50s%n"," ANYTHING \n");
+                         System.out.println("value in int:"+i);
+                         System.out.printf("value in int :%d \n",i);
+                         System.out.printf("value in short :%d \n",s);
+                         System.out.printf("value in byte :%d \n",b);
+                         System.out.printf("value in long :%d \n",l);
+                         System.out.printf("value in float :%.2f \n",f);
+                         System.out.printf("value in bool :%b \n",bool);
+                         System.out.printf("value in double :%.3f \n",d);
+                         System.out.printf("value in character :%c \n",ch);
+
+                         System.out.printf("%03d\t%-10s\t%.2f\n",1,"minahil amjad",3.87);
+
+                         System.out.printf("%03d\t%-10s\t%.2f\n",2,"fatima",3.89);
+                         
+                         
+                        
+
+}
+}
+                          
